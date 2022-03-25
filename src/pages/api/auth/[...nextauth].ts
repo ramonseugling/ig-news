9adf025df46a82607ce513b95bf270f616d1b595
Ramon Seugling <ramonseugling@gmail.com>
2022-03-25 09:22:34 -0300

    Query to create user added

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -14,4 +17,15 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, email, profile }) {
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
