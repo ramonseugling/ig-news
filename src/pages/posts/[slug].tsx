a95ebc4bb53a10c76b5859e523f1a67d3a3a2bac
Ramon Seugling <ramonseugling@gmail.com>
2022-04-08 20:13:46 -0300

    Previews of posts added

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -38,7 +38,7 @@ export default function Post({ post }: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({
   req,
   params,
-}) => {
+}: any) => {
   const session = await getSession({ req });
 
   const { slug } = params;
