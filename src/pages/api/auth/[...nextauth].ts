c87596959e9c60319b4e6ddddec95651de4d13a9
Ramon Seugling <ramonseugling@gmail.com>
2022-08-07 19:41:54 -0300

    Fix object possibly undefined

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -30,7 +30,7 @@ export default NextAuth({
                   q.Get(
                     q.Match(
                       q.Index("user_by_email"),
-                      q.Casefold(session.user.email)
+                      q.Casefold(String(session?.user?.email))
                     )
                   )
                 )
